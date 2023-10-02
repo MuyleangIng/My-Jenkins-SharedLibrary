f0dee313c459014f2e7d556a7966fa2c5e0f753a
Muyleang Ing <116934056+MuyleangIng@users.noreply.github.com>
2023-10-02 16:16:32 +0000

    qwer

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) { 
-  def scriptcontents = libraryResource "co/sen/scripts/linux/${config.name}"    
+  def scriptcontents = libraryResource "co/sen/scripts/${config.name}"    
   writeFile file: "${config.name}", text: scriptcontents 
   sh "chmod a+x ./${config.name}"
 } 
