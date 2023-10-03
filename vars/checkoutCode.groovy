698ecda8cea283c61fc39c77697eb5920c5f3fe7
Muyleang Ing <116934056+MuyleangIng@users.noreply.github.com>
2023-10-03 16:28:00 +0700

    qwer

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,6 +1,9 @@
-def call(String repoUrl, String branch){
-   def workingDir = "${env.WORKSPACE}"
-   sh "git clone ${repoUrl} ${workingDir}"
-   sh "git checkout ${branch}"
-   return workingDir
+def call(String repoUrl, String branch) {
+    // def workingDir = "${env.WORKSPACE}"
+    git branch: ${repoUrl}, url: '${branch}'
+    // sh "git clone ${repoUrl} ${workingDir}"
+    // sh "cd ${workingDir} && git checkout ${branch}"
+    // sh "cd ${workingDir} && git pull origin ${branch}" // Perform git pull
+   
+    // return workingDir
 }
