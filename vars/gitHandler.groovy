3637e1590deceafd36b3e8f3b19ab756bf2e9a45
Muyleang Ing <116934056+MuyleangIng@users.noreply.github.com>
2023-10-04 03:10:36 +0700

    aa1

--- vars/gitHandler.groovy
+++ vars/gitHandler.groovy
@@ -1,39 +1,29 @@
-def call(REPO_URL,CREDENTIAL_GIT, BRANCH, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
-    if (deploy == "production") {
-    echo "Clone from master"
-    try {
-        // Fetch the code from the Git repository
-        git credentialsId: ${CREDENTIAL_GIT}, url: ${REPO_URL}
-        //git branch: ${BRANCH},credentialsId: 'password_for_gitlab', url: ${REPO_URL}
-        sendTelegramMessage("Pull succeeded!")
-        sendGmailMessage("Pull succeeded!")
-    } catch (Exception e) {
-        sendTelegramMessage("Pull failed!")
-        throw e
+def call(REPO_URL, CREDENTIAL_GIT, BRANCH, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
+    if (TEST_CHOICE == "production") {
+        echo "Clone from master"
+        try {
+            // Fetch the code from the Git repository
+            git credentialsId: CREDENTIAL_GIT, url: REPO_URL
+            sendTelegramMessage("Pull succeeded!", TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
+            sendGmailMessage("Pull succeeded!")
+        } catch (Exception e) {
+            sendTelegramMessage("Pull failed!", TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
+            throw e
+        }
+    } else if (TEST_CHOICE == "development") {
+        echo "Clone from main"
+        try {
+            // Fetch the code from the Git repository
+            git branch: BRANCH, credentialsId: CREDENTIAL_GIT, url: REPO_URL
+            sendTelegramMessage("Pull succeeded!", TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
+            sendGmailMessage("Pull succeeded!")
+        } catch (Exception e) {
+            sendTelegramMessage("Pull failed!", TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
+            throw e
+        }
     }
-    } else if (deploy == "development") {
-    echo "Clone from main"
-    try {
-        // Fetch the code from the Git repository
-        git branch: ${BRANCH},credentialsId: ${CREDENTIAL_GIT}, url: ${REPO_URL}
-        //git branch: ${BRANCH}, credentialsId: 'password_for_gitlab', url: ${REPO_URL}
-        sendTelegramMessage("Pull succeeded!")
-        sendGmailMessage("Pull succeeded!")
-    } catch (Exception e) {
-        sendTelegramMessage("Pull failed!")
-        throw e
-    }
-    }
-    // git branch: ${BRANCH}, url: ${REPO_URL}
-    // try {
-    //     // Fetch the code from the Git repository
-    // git branch: ${BRANCH},credentialsId: 'password_for_gitlab', url: ${REPO_URL}
-    // sendTelegramMessage("Pull succeeded!")
-    // } catch (Exception e) {
-    // sendTelegramMessage("Pull failed!")
-    // throw e
-    // }
 }
+
 def sendTelegramMessage(message, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
     sh "curl -s -X POST https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text='${message}'"
 }
