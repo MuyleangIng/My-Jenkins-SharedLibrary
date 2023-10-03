a44867aee39d637f940e5ad8d93fe35f64b435e4
Muyleang Ing <116934056+MuyleangIng@users.noreply.github.com>
2023-10-04 03:01:35 +0700

    aa

--- vars/gitHandler.groovy
+++ vars/gitHandler.groovy
@@ -1,12 +1,12 @@
 // def call(String repoUrl, String branch) {
 //     git branch: branch, url: repoUrl
 // }
-def gitHandler(REPO_URL, BRANCH, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
+def gitHandler(REPO_URL,CREDENTIAL_GIT, BRANCH, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
     if (deploy == "production") {
     echo "Clone from master"
     try {
         // Fetch the code from the Git repository
-        git branch: ${BRANCH}, url: ${REPO_URL}
+        git credentialsId: ${CREDENTIAL_GIT}, url: ${REPO_URL}
         //git branch: ${BRANCH},credentialsId: 'password_for_gitlab', url: ${REPO_URL}
         sendTelegramMessage("Pull succeeded!")
         sendGmailMessage("Pull succeeded!")
@@ -18,7 +18,7 @@ def gitHandler(REPO_URL, BRANCH, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
     echo "Clone from main"
     try {
         // Fetch the code from the Git repository
-        git branch: ${BRANCH}, url: ${REPO_URL}
+        git branch: ${BRANCH},credentialsId: ${CREDENTIAL_GIT}, url: ${REPO_URL}
         //git branch: ${BRANCH}, credentialsId: 'password_for_gitlab', url: ${REPO_URL}
         sendTelegramMessage("Pull succeeded!")
         sendGmailMessage("Pull succeeded!")
@@ -28,6 +28,14 @@ def gitHandler(REPO_URL, BRANCH, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
     }
     }
     // git branch: ${BRANCH}, url: ${REPO_URL}
+    // try {
+    //     // Fetch the code from the Git repository
+    // git branch: ${BRANCH},credentialsId: 'password_for_gitlab', url: ${REPO_URL}
+    // sendTelegramMessage("Pull succeeded!")
+    // } catch (Exception e) {
+    // sendTelegramMessage("Pull failed!")
+    // throw e
+    // }
 }
 def sendTelegramMessage(message, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
     sh "curl -s -X POST https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text='${message}'"
