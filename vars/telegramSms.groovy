6be30515d00c9cffb1175a3583136175a1a07412
Muyleang Ing <116934056+MuyleangIng@users.noreply.github.com>
2023-10-03 16:08:46 +0700

    sddsd

--- vars/telegramSms.groovy
+++ vars/telegramSms.groovy
@@ -1,6 +1,3 @@
-// def sendTelegramMessage(message) {
-//     sh "curl -s -X POST https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text='${message}'"
-// }
 def sendTelegramMessage(Map<String, Object> envVars, String message) {
     def telegramBotToken = envVars.TELEGRAM_BOT_TOKEN
     def telegramChatId = envVars.TELEGRAM_CHAT_ID
