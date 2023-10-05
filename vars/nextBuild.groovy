0d0c8a7380e40e16a1f492a01024f865481e7893
Muyleang Ing <116934056+MuyleangIng@users.noreply.github.com>
2023-10-05 03:23:08 +0000

    new update with next

---
+++ vars/nextBuild.groovy
@@ -0,0 +1,48 @@
+def call(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, Docker_Tag, MAIL_SEND_TO, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
+    cleanDockerImages(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, Docker_Tag)
+    
+    try {
+        buildDockerImage(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, Docker_Tag)
+        sendTelegramMessage("Docker build Successfully!")
+    } catch (Exception e) {
+        echo "Build failed, retrying..."
+        cleanDockerImages(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, Docker_Tag)
+        buildDockerImage(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, Docker_Tag)
+        sendTelegramMessage("Docker build failed!")
+        throw e
+    }
+}
+
+def cleanDockerImages(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, Docker_Tag) {
+    sh """
+        docker rmi -f ${BUIDL_CONTAINER_NAME}:${Docker_Tag}
+        docker rmi -f ${REGISTRY_DOCKER}/${BUIDL_CONTAINER_NAME}:${Docker_Tag}
+    """
+}
+
+def buildDockerImage(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, Docker_Tag) {
+    sh "docker build -t ${BUIDL_CONTAINER_NAME}:${Docker_Tag} -t ${REGISTRY_DOCKER}/${BUIDL_CONTAINER_NAME}:${Docker_Tag} ."
+}
+
+def sendTelegramMessage(message) {
+    sh "curl -s -X POST https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text='${message}'"
+}
+
+def sendGmailMessage(message) {
+    mail bcc: '', body: message, cc: '', from: '', replyTo: '', subject: 'Docker Build Status', to: MAIL_SEND_TO  
+}
+
+// def dockerfileContent = '''
+// # Dockerfile
+// FROM node:14 as build
+// WORKDIR /app
+// COPY ./ ./
+// RUN npm install --force
+// RUN npm run build
+
+// FROM nginx:1.23.2
+// COPY --from=build /app/build /usr/share/nginx/html
+
+// EXPOSE 80
+// CMD ["nginx", "-g", "daemon off;"]
+// '''
---
+++ vars/nextDeploy.groovy
@@ -0,0 +1,108 @@
+def call(minPort, maxPort, REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, Docker_Tag, MAIL_SEND_TO, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
+    def minPortValue = minPort.toInteger()
+    def maxPortValue = maxPort.toInteger()
+    def selectedPort = selectRandomAvailablePort(minPortValue, maxPortValue)
+
+    if (selectedPort) {
+        echo "Selected port: $selectedPort"
+        sh "docker run -d -p $selectedPort:3000 ${REGISTRY_DOCKER}/${BUIDL_CONTAINER_NAME}:${Docker_Tag}"
+        sendTelegramMessage("Docker Deploy $selectedPort:3000 Successfully!", TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
+        sendGmailMessage("Docker Deploy $selectedPort:3000 Successfully!", MAIL_SEND_TO)
+    } else {
+        error "No available ports found in the range $minPort-$maxPort"
+    }
+
+    def usedPorts = listPortsInUseForDocker(minPortValue, maxPortValue)
+    if (!usedPorts.isEmpty()) {
+        echo "Ports already in use for Docker port mapping on port 3000: ${usedPorts.join(', ')}"
+        sendTelegramMessage("Ports already in use for Docker port mapping on port 3000: ${usedPorts.join(', ')}", TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
+    }
+    // def usedPorts = listPortsInUseForDocker(minPortValue, maxPortValue)
+
+    // if (!usedPorts.isEmpty()) {
+    // // Check if 80 is in the used ports
+    // if (usedPorts.contains(80)) {
+    //     echo "Ports already in use for Docker port mapping on port 80: ${usedPorts.join(', ')}"
+    //     sendTelegramMessage("Ports already in use for Docker port mapping on port 80: ${usedPorts.join(', ')}", TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
+    // }
+
+    // // Check if 3000 is in the used ports
+    // if (usedPorts.contains(3000)) {
+    //     echo "Ports already in use for Docker port mapping on port 3000: ${usedPorts.join(', ')}"
+    //     sendTelegramMessage("Ports already in use for Docker port mapping on port 3000: ${usedPorts.join(', ')}", TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
+    // }
+
+    // // Check if 8080 is in the used ports
+    // if (usedPorts.contains(8080)) {
+    //     echo "Ports already in use for Docker port mapping on port 8080: ${usedPorts.join(', ')}"
+    //     sendTelegramMessage("Ports already in use for Docker port mapping on port 8080: ${usedPorts.join(', ')}", TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
+    // }
+    // }
+}
+
+def sendTelegramMessage(message, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
+    sh "curl -s -X POST https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text='${message}'"
+}
+
+def sendGmailMessage(message, MAIL_SEND_TO) {
+     mail bcc: '', 
+     body: message, cc: '', 
+     from: '', replyTo: '', 
+     subject: 'Hello', 
+     to: MAIL_SEND_TO
+}
+
+def selectRandomAvailablePort(minPort, maxPort) {
+    def numberOfPortsToCheck = maxPort - minPort + 1
+    def portsToCheck = (minPort..maxPort).toList()
+    Collections.shuffle(portsToCheck)
+
+    for (int i = 0; i < numberOfPortsToCheck; i++) {
+        def portToCheck = portsToCheck[i]
+        if (isPortAvailable(portToCheck) && !isPortInUseForDocker(portToCheck)) {
+            return portToCheck
+        }
+    }
+    return null
+}
+
+def isPortAvailable(port) {
+    def socket
+    try {
+        socket = new Socket("localhost", port)
+        return false // Port is already in use
+    } catch (Exception e) {
+        return true // Port is available
+    } finally {
+        if (socket) {
+            socket.close()
+        }
+    }
+}
+
+def isPortInUseForDocker(port) {
+    def dockerPsOutput = sh(script: "docker ps --format '{{.Ports}}'", returnStdout: true).trim()
+
+    // Check if the Docker container port mapping contains ":$port->80/tcp"
+    return dockerPsOutput.contains(":$port->3000/tcp")
+}
+// def isPortInUseForDocker(port) {
+//     def dockerPsOutput = sh(script: "docker ps --format '{{.Ports}}'", returnStdout: true).trim()
+
+//     // Check if the Docker container port mapping contains ":$port->80/tcp",
+//     // ":$port->3000/tcp", or ":$port->8080/tcp"
+//     return dockerPsOutput.contains(":$port->80/tcp") ||
+//            dockerPsOutput.contains(":$port->3000/tcp") ||
+//            dockerPsOutput.contains(":$port->8080/tcp")
+// }
+
+
+def listPortsInUseForDocker(minPort, maxPort) {
+    def usedPorts = []
+    for (int port = minPort; port <= maxPort; port++) {
+        if (isPortInUseForDocker(port)) {
+            usedPorts.add(port)
+        }
+    }
+    return usedPorts
+}
