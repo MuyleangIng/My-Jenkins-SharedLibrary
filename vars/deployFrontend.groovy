049a95fec62521c9606dec192c5bce14f80df864
Muyleang Ing <116934056+MuyleangIng@users.noreply.github.com>
2023-11-03 04:21:40 +0000

    update

--- vars/deployFrontend.groovy
+++ vars/deployFrontend.groovy
@@ -1,11 +1,11 @@
-def call(minPort, maxPort, REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, DOCKER_TAG, MAIL_SEND_TO, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID ) {
+def call(minPort, maxPort, REGISTRY_DOCKER, BUIDL_CONTAINER_NAME,CONTAINER_NAME, DOCKER_TAG, MAIL_SEND_TO, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID ) {
     def minPortValue = minPort.toInteger()
     def maxPortValue = maxPort.toInteger()
     def selectedPort = selectRandomAvailablePort(minPortValue, maxPortValue)
 
     if (selectedPort) {
         echo "Selected port: $selectedPort"
-        sh "docker run -d -p $selectedPort:80 --name ${BUIDL_CONTAINER_NAME} ${REGISTRY_DOCKER}/${BUIDL_CONTAINER_NAME}:${DOCKER_TAG}"
+        sh "docker run -d -p $selectedPort:80 --name ${CONTAINER_NAME} ${REGISTRY_DOCKER}/${BUIDL_CONTAINER_NAME}:${DOCKER_TAG}"
         sendTelegramMessage("Docker Deploy $selectedPort:80 Successfully!", TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
         sendGmailMessage("Docker Deploy $selectedPort:80 Successfully!", MAIL_SEND_TO)
         def ipAddress = sh(script: 'curl -s ifconfig.me', returnStdout: true).trim()
