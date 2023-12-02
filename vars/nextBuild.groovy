ef0f4084852830eb47addb9cc718becf69fb3abf
MuyleangIng <muyleanging@gmail.com>
2023-12-02 21:52:44 +0700

    Remove unused function sendTelegramMessage()

--- vars/nextBuild.groovy
+++ vars/nextBuild.groovy
@@ -26,4 +26,3 @@ def buildDockerImage(registryName, imageName, imageTag) {
 def sendTelegramMessage(message) {
     sh "curl -s -X POST https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text='${message}'"
 }
-
