30f787a72b127c144d4323ca2cf37e96923559ef
MuyleangIng <muyleanging@gmail.com>
2023-12-02 21:52:29 +0700

    Refactor buildDockerImage and cleanDockerImages functions

--- vars/nextBuild.groovy
+++ vars/nextBuild.groovy
@@ -1,48 +1,29 @@
-def call(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, DOCKER_TAG, MAIL_SEND_TO, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
-    cleanDockerImages(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, DOCKER_TAG)
+def call(registryName, imageName, imageTag,TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
+    cleanDockerImages(registryName, imageName, imageTag)
     
     try {
-        buildDockerImage(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, DOCKER_TAG)
+        buildDockerImage(registryName, imageName, imageTag)
         sendTelegramMessage("Docker build Successfully!")
     } catch (Exception e) {
         echo "Build failed, retrying..."
-        cleanDockerImages(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, DOCKER_TAG)
-        buildDockerImage(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, DOCKER_TAG)
+        cleanDockerImages(registryName, imageName, imageTag)
+        buildDockerImage(registryName, imageName, imageTag)
         sendTelegramMessage("Docker build failed!")
         throw e
     }
 }
 
-def cleanDockerImages(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, DOCKER_TAG) {
+def cleanDockerImages(registryName, imageName, imageTag) {
     sh """
-        docker rmi -f ${BUIDL_CONTAINER_NAME}:${DOCKER_TAG}
-        docker rmi -f ${REGISTRY_DOCKER}/${BUIDL_CONTAINER_NAME}:${DOCKER_TAG}
+        docker rmi -f ${registryName}/${imageName}:${imageTag}
     """
 }
 
-def buildDockerImage(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, DOCKER_TAG) {
-    sh "docker build -t ${BUIDL_CONTAINER_NAME}:${DOCKER_TAG} -t ${REGISTRY_DOCKER}/${BUIDL_CONTAINER_NAME}:${DOCKER_TAG} ."
+def buildDockerImage(registryName, imageName, imageTag) {
+    sh "docker build -t ${registryName}/${imageName}:${imageTag} ."
 }
 
 def sendTelegramMessage(message) {
     sh "curl -s -X POST https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text='${message}'"
 }
 
-def sendGmailMessage(message) {
-    mail bcc: '', body: message, cc: '', from: '', replyTo: '', subject: 'Docker Build Status', to: MAIL_SEND_TO  
-}
-
-// def dockerfileContent = '''
-// # Dockerfile
-// FROM node:14 as build
-// WORKDIR /app
-// COPY ./ ./
-// RUN npm install --force
-// RUN npm run build
-
-// FROM nginx:1.23.2
-// COPY --from=build /app/build /usr/share/nginx/html
-
-// EXPOSE 80
-// CMD ["nginx", "-g", "daemon off;"]
-// '''
